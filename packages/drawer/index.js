880db7743c4bd2d55e041a1c568875d26c1c9810
packer-陈星星 <13246190524@163.com>
2019-03-13 17:45:44 +0800

    添加抽屉组件

---
+++ packages/drawer/index.js
@@ -0,0 +1,43 @@
+// packages/drawer/index.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    // 抽屉的方向，可选值：left right
+    type:{
+      type:String,
+      value:''
+    },
+    // 是否显示遮罩层
+    mask:{
+      type:Boolean,
+      value:true
+    },
+    // 是否允许点击遮罩层关闭
+    maskClosable:{
+      type:Boolean,
+      value:true
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    closeDrawer(){
+      if(this.data.maskClosable){
+        this.setData({
+          type:''
+        })
+      }
+    }
+  }
+})
